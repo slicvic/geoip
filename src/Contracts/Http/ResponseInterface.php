23688bb10826fcf4a53f3a77f6bc9d006aa2cad3
victor <dot@mymngmt.com>
2017-02-09 15:52:19 -0500

    Interface for HTTP client response

---
+++ src/Contracts/Http/ResponseInterface.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Slicvic\Geoip\Contracts\Http;
+
+/**
+ * Interface for HTTP responses.
+ */
+interface ResponseInterface
+{
+    /**
+     * Get HTTP status code.
+     *
+     * @return int
+     */
+    public function getStatusCode();
+
+    /**
+     * Set HTTP status code.
+     *
+     * @param  int $code
+     * @return $this
+     */
+    public function setStatusCode($code);
+
+    /**
+     * Get response headers.
+     *
+     * @return array
+     */
+    public function getHeaders();
+
+    /**
+     * Set response headers.
+     *
+     * @param  array $headers
+     * @return $this
+     */
+    public function setHeaders(array $headers);
+
+    /**
+     * Get response body.
+     *
+     * @return string
+     */
+    public function getBody();
+
+    /**
+     * Set response body.
+     *
+     * @param  string $body
+     * @return $this
+     */
+    public function setBody($body);
+}
