4baa437ed3519a57d7df1e005271da29f0814d87
victor <dot@mymngmt.com>
2017-02-10 09:45:21 -0500

    Test for curl HTTP client

---
+++ tests/Unit/Http/Clients/CurlTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Slicvic\Geoip\Test\Unit\Http;
+
+use PHPUnit\Framework\TestCase;
+use Slicvic\Geoip\Http\Clients\Curl;
+use Slicvic\Geoip\Http\Response;
+
+class CurlTest extends TestCase
+{
+    public function testGetSuccess()
+    {
+        $response = Curl::get('http://www.example.com/');
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertContains('This domain is established to be used for illustrative examples in documents.', $response->getBody());
+    }
+
+    public function testGetNotFound()
+    {
+        $response = Curl::get('http://www.example.com/space%20here.html');
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertSame(404, $response->getStatusCode());
+        $this->assertContains('404 - Not Found', $response->getBody());
+    }
+}
