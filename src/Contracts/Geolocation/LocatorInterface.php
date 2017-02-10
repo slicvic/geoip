3a4e310dfda085e17c08b198bc293bd854664a21
victor <dot@mymngmt.com>
2017-02-10 10:02:58 -0500

    Move locator interface

---
+++ src/Contracts/Geolocation/LocatorInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Slicvic\Geoip\Contracts\Geolocation;
+
+/**
+ * Interface for geolocation clients.
+ *
+ * @package Slicvic\Geoip\Contracts\Geolocation
+ */
+interface LocatorInterface
+{
+    /**
+     * Find location by IP address.
+     *
+     * @param  string $ip
+     * @return \Slicvic\Geoip\Contracts\Geolocation\ResponseInterface
+     */
+    public function locate($ip);
+}
