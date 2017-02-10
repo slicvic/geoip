4665feb6ac8817ed9f89a6cfeb49a0a2423f9403
victor <dot@mymngmt.com>
2017-02-10 10:03:53 -0500

    Interface for geolocation clients response

---
+++ src/Contracts/Geolocation/ResponseInterface.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Slicvic\Geoip\Contracts\Geolocation;
+
+/**
+ * Interface for HTTP responses.
+ *
+ * @package Slicvic\Geoip\Contracts\Geolocation
+ */
+interface ResponseInterface
+{
+    /**
+     * Get HTTP response.
+     *
+     * @return Slicvic\Geoip\Contracts\Http\ResponseInterface
+     */
+    public function getHttpResponse();
+
+    /**
+     * Get IP address.
+     *
+     * @return string
+     */
+    public function getIp();
+
+    /**
+     * Get city name.
+     *
+     * @return string
+     */
+    public function getCity();
+
+    /**
+     * Get region name.
+     *
+     * @return string
+     */
+    public function getRegion();
+
+    /**
+     * Get country name.
+     *
+     * @return string
+     */
+    public function getCountry();
+
+    /**
+     * Get postal code.
+     *
+     * @return string
+     */
+    public function getPostal();
+
+    /**
+     * Get latitude.
+     *
+     * @return string
+     */
+    public function getLatitude();
+
+    /**
+     * Get longitude.
+     *
+     * @return string
+     */
+    public function getLongitude();
+}
