275bbb40cb55e946c09eecae9455eff0884063ea
vagrant <vagrant@homestead>
2017-02-10 13:48:26 +0000

    Implement curl HTTP client

---
+++ src/Http/Clients/AbstractClient.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Slicvic\Geoip\Http\Clients;
+
+use Slicvic\Geoip\Contracts\Http\ClientInterface;
+
+/**
+ * Abstract base class for HTTP clients.
+ */
+abstract class AbstractClient implements ClientInterface {}
---
+++ src/Http/Clients/Curl.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Slicvic\Geoip\Http\Clients;
+
+use Slicvic\Geoip\Http\Response;
+
+/**
+ * A cURL HTTP client.
+ */
+class Curl extends AbstractClient
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function get($url, array $params = [])
+    {
+        $queryString = (count($params)) ? '?' . http_build_query($params) : '';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url . $queryString);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $body = curl_exec($ch);
+        $headers = curl_getinfo($ch);
+        curl_close($ch);
+
+        $response = new Response($body, $headers['http_code'], $headers);
+        return $response;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function post($url, array $params = [])
+    {
+        //
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function put($url, array $params = [])
+    {
+        //
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function delete($url, array $params = [])
+    {
+        //
+    }
+}
