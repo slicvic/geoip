4d6603c541db51eead7036e7aae923ed411d1119
victor <dot@mymngmt.com>
2017-02-11 09:15:12 -0500

    Add test case for invalid url

--- tests/Unit/Http/Clients/CurlTest.php
+++ tests/Unit/Http/Clients/CurlTest.php
@@ -15,7 +15,7 @@ class CurlTest extends TestCase
         $this->curl = new Curl();
     }
 
-    public function testGetSuccess()
+    public function testGet200()
     {
         $response = $this->curl->get('http://www.example.com/');
         $this->assertInstanceOf(Response::class, $response);
@@ -23,11 +23,19 @@ class CurlTest extends TestCase
         $this->assertContains('This domain is established to be used for illustrative examples in documents.', $response->getBody());
     }
 
-    public function testGetNotFound()
+    public function testGet404()
     {
         $response = $this->curl->get('http://www.example.com/space%20here.html');
         $this->assertInstanceOf(Response::class, $response);
         $this->assertSame(404, $response->getStatusCode());
         $this->assertContains('404 - Not Found', $response->getBody());
+    }
+
+    public function testGetInvalidUrl()
+    {
+        $response = $this->curl->get('http//www.example.com/');
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertSame(0, $response->getStatusCode());
+        $this->assertSame('', $response->getBody());
     }
 }
