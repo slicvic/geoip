909a5cc52b5393cbd46854740d94b1f410b77a53
victor <dot@mymngmt.com>
2017-02-12 21:50:29 -0500

    Update class description

--- src/Geolocation/Clients/IpInfo.php
+++ src/Geolocation/Clients/IpInfo.php
@@ -5,7 +5,7 @@ namespace Slicvic\Geoip\Geolocation\Clients;
 use Slicvic\Geoip\Geolocation\Response as GeoResponse;
 
 /**
- * Client for ipinfo.io IP lookup API.
+ * Client for http://ipinfo.io IP lookup API.
  */
 class IpInfo extends AbstractClient
 {
