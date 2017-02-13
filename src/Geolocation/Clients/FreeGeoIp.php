69da601357ce5255c3834205174e4eac3581ab58
victor <dot@mymngmt.com>
2017-02-12 22:09:49 -0500

    Implement freegeoip.net client

---
+++ src/Geolocation/Clients/FreeGeoIp.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Slicvic\Geoip\Geolocation\Clients;
+
+use Slicvic\Geoip\Geolocation\Response as GeoResponse;
+
+/**
+ * Client for https://freegeoip.net/ IP geolocation API.
+ */
+class FreeGeoIp extends AbstractClient
+{
+    const API_URL = 'http://freegeoip.net/json';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function locate($ip)
+    {
+        $httpResponse = $this->httpClient->get(static::API_URL . '/' . $ip);
+
+        if ($httpResponse->getStatusCode() !== 200) {
+            return GeoResponse::create($httpResponse, $ip);
+        }
+
+        $locationData = json_decode($httpResponse->getBody(), true);
+
+        return GeoResponse::create(
+            $httpResponse,
+            $locationData['ip'],
+            $locationData['city'],
+            $locationData['region_name'],
+            $locationData['country_code'],
+            $locationData['zip_code'],
+            $locationData['latitude'],
+            $locationData['longitude']
+        );
+    }
+}
