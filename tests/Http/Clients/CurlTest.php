ed898efddb66186e2f973d494f3a428d8e806db0
victor <dot@mymngmt.com>
2017-02-14 09:14:10 -0500

    Refactor test

--- tests/Http/Clients/CurlTest.php
+++ tests/Http/Clients/CurlTest.php
@@ -3,8 +3,8 @@
 namespace Slicvic\Geoip\Test\Http\Clients;
 
 use PHPUnit\Framework\TestCase;
+use Slicvic\Geoip\Contracts\Http\ResponseInterface;
 use Slicvic\Geoip\Http\Clients\Curl;
-use Slicvic\Geoip\Http\Response;
 
 class CurlTest extends TestCase
 {
@@ -18,7 +18,7 @@ class CurlTest extends TestCase
     public function testGetRespondsWith200()
     {
         $response = $this->curl->get('http://www.example.com/');
-        $this->assertInstanceOf(Response::class, $response);
+        $this->assertInstanceOf(ResponseInterface::class, $response);
         $this->assertSame(200, $response->getStatusCode());
         $this->assertContains('This domain is established to be used for illustrative examples in documents.', $response->getBody());
     }
@@ -26,7 +26,7 @@ class CurlTest extends TestCase
     public function testGetRespondsWith404()
     {
         $response = $this->curl->get('http://www.iana.org/omains');
-        $this->assertInstanceOf(Response::class, $response);
+        $this->assertInstanceOf(ResponseInterface::class, $response);
         $this->assertSame(404, $response->getStatusCode());
         $this->assertContains('This page does not exist', $response->getBody());
     }
