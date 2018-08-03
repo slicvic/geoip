1dd6b74503423954ca46a256261fd675359abbf8
vagrant <vagrant@homestead>
2018-08-03 01:23:45 +0000

    Add better error checking

--- src/Locator/IpInfo.php
+++ src/Locator/IpInfo.php
@@ -21,17 +21,17 @@ class IpInfo extends AbstractLocator
         if (200 === $httpResponse->getStatusCode()) {
             $data = json_decode($httpResponse->getBody(), true);
 
-            if (isset($data['ip'])) {
-                list($lat, $lon) = isset($data['loc']) ? explode(',', $data['loc']) : ['', ''];
+            if (!empty($data['ip'])) {
+                $latlng = !empty($data['loc']) ? explode(',', $data['loc']) : null;
                 return (new Response(
                     $httpResponse,
                     $data['ip'],
-                    isset($data['city']) ? $data['city'] : '',
-                    isset($data['region']) ? $data['region'] : '',
-                    isset($data['country']) ? $data['country'] : '',
-                    isset($data['postal']) ? $data['postal'] : '',
-                    $lat,
-                    $lon
+                    !empty($data['city']) ? $data['city'] : '',
+                    !empty($data['region']) ? $data['region'] : '',
+                    !empty($data['country']) ? $data['country'] : '',
+                    !empty($data['postal']) ? $data['postal'] : '',
+                    !empty($latlng[0]) ? $latlng[0] : '',
+                    !empty($latlng[1]) ? $latlng[1] : ''
                 ));
             } else {
                 return (new Response($httpResponse, $ip));
